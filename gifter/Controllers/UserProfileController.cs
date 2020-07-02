46667cf571405e5367bf59aa58fb1e9a4f43bd04
unknown <buckleyderekp@gmail.com>
2020-07-02 10:43:27 -0500

    added user profile and comment

--- gifter/Controllers/UserProfileController.cs
+++ gifter/Controllers/UserProfileController.cs
@@ -7,24 +7,24 @@ namespace Gifter.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class PostController : ControllerBase
+    public class UserProfileController : ControllerBase
     {
-        private readonly PostRepository _postRepository;
-        public PostController(ApplicationDbContext context)
+        private readonly UserProfileRepository _profileRepository;
+        public UserProfileController(ApplicationDbContext context)
         {
-            _postRepository = new PostRepository(context);
+            _profileRepository = new UserProfileRepository(context);
         }
 
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok(_postRepository.GetAll());
+            return Ok(_profileRepository.GetAll());
         }
 
         [HttpGet("{id}")]
         public IActionResult Get(int id)
         {
-            var post = _postRepository.GetById(id);
+            var post = _profileRepository.GetById(id);
             if (post == null)
             {
                 return NotFound();
@@ -32,34 +32,29 @@ namespace Gifter.Controllers
             return Ok(post);
         }
 
-        [HttpGet("getbyuser/{id}")]
-        public IActionResult GetByUser(int id)
-        {
-            return Ok(_postRepository.GetByUserProfileId(id));
-        }
         [HttpPost]
-        public IActionResult Post(Post post)
+        public IActionResult UserProfile(UserProfile post)
         {
-            _postRepository.Add(post);
+            _profileRepository.Add(post);
             return CreatedAtAction("Get", new { id = post.Id }, post);
         }
 
         [HttpPut("{id}")]
-        public IActionResult Put(int id, Post post)
+        public IActionResult Put(int id, UserProfile post)
         {
             if (id != post.Id)
             {
                 return BadRequest();
             }
 
-            _postRepository.Update(post);
+            _profileRepository.Update(post);
             return NoContent();
         }
 
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
-            _postRepository.Delete(id);
+            _profileRepository.Delete(id);
             return NoContent();
         }
     }
--- gifter/Repositories/PostRepository.cs
+++ gifter/Repositories/PostRepository.cs
@@ -32,5 +32,25 @@ namespace Gifter.Repositories
                             .ToList();
         }
 
+        public void Add(Post post)
+        {
+            _context.Add(post);
+            _context.SaveChanges();
+        }
+
+        public void Update(Post post)
+        {
+            _context.Entry(post).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var post = GetById(id);
+            _context.Post.Remove(post);
+            _context.SaveChanges();
+        }
+
+
     }
 }
